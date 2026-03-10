3d3062d34ccf0db847f2c23373355fbb1f7920c5
StreamDotMy <sales@stream.my>
2026-03-10 16:55:46 +0800

    update meta tags n footer

--- frontend/src/views/components/PublicFooter.js
+++ frontend/src/views/components/PublicFooter.js
@@ -4,8 +4,8 @@ const PublicFooter = () => {
     return (
         <footer className="public-footer mt-auto">
             <div className="container d-flex justify-content-between align-items-center">
-                <div>&copy;</div>
-                <div>Selangor Islamic Religious Department</div>
+                <div>&copy; 2026 ENFORCEMENT SYSTEM ( i - SYAEMS ) </div>
+                <div> Selangor Islamic Religious Department</div>
             </div>
         </footer>
     );
